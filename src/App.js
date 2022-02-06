ca80deb47d76bede9d6f637b57ba1f0f53f5e0cb
kislaya1996 <kislayamishra96@gmail.com>
2022-02-06 21:43:54 +0530

    updated to use the conenction

--- src/App.js
+++ src/App.js
@@ -1,22 +1,106 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import Peer from 'peerjs';
+import { useEffect, useState } from 'react';
+import { Box } from '@mui/material';
+import { Button, Typography } from '@mui/material';
 function App() {
+  const [peer, setPeer] = useState(new Peer())
+  const [peerId, setPeerId] = useState('')
+  const [conn, setConn] = useState({})
+  const [opponentId, setOpponentId] = useState('')
+  const [turn, setTurn] = useState(true)
+
+  function initializePeer(){
+    peer.on('open',function(id){
+      setPeerId(id)
+      console.log(id)
+    })
+    peer.on('error', function(error){
+      alert(error)
+    })
+  }
+
+  useEffect(()=>{
+    initializePeer()
+  },[])
+
+
+  function initConnection(conn){
+    conn.on('open', function(){
+      conn.on('data', function(data){
+        console.log(data)
+      })
+    })
+    conn.on('error', function(error){
+      console.log(error)
+    })
+  }
+  
+
+  function startGame(){
+    alert("Game Id:  "+peerId)
+    peer.on('connection', function(conn) { 
+      setConn(conn)
+     initConnection(conn)
+    });
+   
+  }
+
+  function joinGame(){
+    let oppId = prompt("Enter Room ID:")
+    var conn = peer.connect(oppId);
+    setConn(conn)
+    initConnection(conn)
+  }
+
+  function sendData(){
+    console.log(conn.open)
+    conn.send({'random': peerId})
+    conn.on('open', function() {
+      console.log(conn)
+     })
+
+     conn.on('error', function(error){
+       console.log(error)
+     })
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Box sx={{
+          py:6
+        }}>
+         <Typography variant="h1" component="div" gutterBottom>
+        TIC-STACK-TOE
+      </Typography>
+        </Box>
+       <Box sx={{
+         display:"flex",
+         justifyContent:"space-around"
+       }}>
+         <Box sx={{
+           px:2
+         }}>
+
+        <Button variant='contained' onClick={startGame}>
+          Start Game
+        </Button>
+         </Box>
+        <Box sx={{
+          px:2
+        }}>
+
+        <Button variant="contained" onClick={joinGame}>
+          Join Game
+        </Button>
+
+        <Button variant="contained" onClick={sendData}>
+          Test
+        </Button>
+        </Box>
+       </Box>
       </header>
     </div>
   );

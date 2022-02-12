d1994eeef678f6b706591749f6731ff8427c0e8b
kislaya1996 <kislayamishra96@gmail.com>
2022-02-13 02:50:44 +0530

    added board UI

--- src/App.js
+++ src/App.js
@@ -3,13 +3,20 @@ import './App.css';
 import Peer from 'peerjs';
 import { useEffect, useState } from 'react';
 import { Box } from '@mui/material';
-import { Button, Typography } from '@mui/material';
+import { Button, Typography, Grid, Container, IconButton } from '@mui/material';
+import { styled } from '@mui/material/styles';
+import ArrowUpwardIcon from '@mui/icons-material/ArrowUpward';
+
 function App() {
   const [peer, setPeer] = useState(new Peer())
   const [peerId, setPeerId] = useState('')
   const [conn, setConn] = useState({})
-  const [opponentId, setOpponentId] = useState('')
+  const [opponentId, setOpponentId] = useState(null)
   const [turn, setTurn] = useState(true)
+  const [openGameBoard, setOpenGameBoard] = useState(false)
+  const [board, setBoard] = useState([
+   ['','','','','','','','','',''],['','','','','','','','','',''],['','','','','','','','','',''],['','','','','','','','','',''],['','','','','','','','','',''],['','','','','','','','','','']
+  ])
 
   function initializePeer(){
     peer.on('open',function(id){
@@ -25,11 +32,33 @@ function App() {
     initializePeer()
   },[])
 
+  useEffect(()=>{
+    if(Object.keys(conn).length==0)
+    {
+      return
+    }
+    begin()
+  },[conn])
 
-  function initConnection(conn){
+  useEffect(()=>{
+    if(opponentId===null)
+    {
+      return
+    }
+    console.log("open game board")
+    setOpenGameBoard(true)
+  },[opponentId])
+
+
+  function begin(){
+    console.log('begins')
     conn.on('open', function(){
+      conn.send({'id':peerId})
       conn.on('data', function(data){
         console.log(data)
+        if(data.hasOwnProperty('id')){
+          setOpponentId(data['id'])
+        }
       })
     })
     conn.on('error', function(error){
@@ -41,8 +70,7 @@ function App() {
   function startGame(){
     alert("Game Id:  "+peerId)
     peer.on('connection', function(conn) { 
-      setConn(conn)
-     initConnection(conn)
+    setConn(conn)
     });
    
   }
@@ -50,31 +78,25 @@ function App() {
   function joinGame(){
     let oppId = prompt("Enter Room ID:")
     var conn = peer.connect(oppId);
-    setConn(conn)
-    initConnection(conn)
+    setConn(conn) 
+    setTurn(false)
   }
 
   function sendData(){
-    console.log(conn.open)
     conn.send({'random': peerId})
-    conn.on('open', function() {
-      console.log(conn)
-     })
-
-     conn.on('error', function(error){
-       console.log(error)
-     })
   }
+
+
 
   return (
     <div className="App">
-      <header className="App-header">
+      {!openGameBoard && <header className="App-header">
         <Box sx={{
           py:6
         }}>
          <Typography variant="h1" component="div" gutterBottom>
-        TIC-STACK-TOE
-      </Typography>
+          TIC-STACK-TOE
+         </Typography>
         </Box>
        <Box sx={{
          display:"flex",
@@ -96,12 +118,55 @@ function App() {
           Join Game
         </Button>
 
-        <Button variant="contained" onClick={sendData}>
-          Test
-        </Button>
         </Box>
        </Box>
-      </header>
+      </header>}
+
+      {openGameBoard && 
+      <Container >
+          <Box sx={{ width: '100%', pl:10, pt:10 }}>
+            {
+              [...Array(6).keys()].map((item, i) => (
+                <Grid container  key={i} sx={{width:'100%'}}>
+                {
+                  [...Array(10).keys()].map((item, j) => (
+                    <Grid item >
+                    <Box className="grid-item" key={j} sx={{
+                      height:'30px',
+                      width:'40px'
+                    }}>
+                        {board[i][j]}
+                    </Box>
+                    </Grid>
+                )) 
+                }
+                </Grid>
+              ))
+            }
+            <Grid container   sx={{width:'100%', }}>
+            {
+            
+            [...Array(10).keys()].map((item, j) => (
+              <Grid item >
+              <Box  key={j} sx={{
+                      fontSize:'30px',
+                      padding:'9px',
+                      textAlign:'center'
+                    }} >
+                      <Button variant='contained' color="primary">
+
+                 <ArrowUpwardIcon/>
+                      </Button>
+              </Box>
+              </Grid>
+              
+          ))}
+          </Grid>
+          
+          </Box>
+      </Container>
+
+      }
     </div>
   );
 }
